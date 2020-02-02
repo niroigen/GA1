0c5e0e62f964cad0d45371ca392b441141dec3b2
Niroigen Suntharam <niro@smarthalo.bike>
2020-02-02 14:39:51 -0500

    tuning

--- ea.h
+++ ea.h
@@ -12,7 +12,7 @@
 
 static const int POPULATION_SIZE = 1000;
 static const int MAX_PARENT_SIZE = 100;
-static const int MAX_OFFSPRING_SIZE = 400;
+static const int MAX_OFFSPRING_SIZE = 200;
 static const int MAX_GENERATIONS = 5000;
 static const float MUTATION_RATE = 0.25;
 
